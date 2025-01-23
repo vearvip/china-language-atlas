239cc989aaa522cea69290740cb5c43509c00f25
唯二 <vear.vip@qq.com>
2025-01-23 03:53:11 +0000

    调试

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+   <div class="section">
+        <h1>中国语言地图集</h1> 
+
+        <p>A1 <a href="A1.JPG">中国语言图</a></p>
+        <p>A2 <a href="A2.JPG">中国汉语方言图</a></p>
+        <p>A3 <a href="A3.JPG">中国少数民族分布图</a></p>
+        <p>A4 <a href="A4.JPG">中国少数民族语言分布图</a></p>
+        <p>A5 <a href="A5.JPG">广西壮族自治区语言分布图</a></p>
+
+        <p>B1 <a href="B1.JPG">官话之一</a><span>黑龙江省 吉林省 辽宁省 内蒙古自治区东部</span></p>
+        <p>B2 <a href="B2.JPG">官话之二 </a><span>北京市 天津市 河北省 山东省西部</span></p>
+        <p>B3 <a href="B3.JPG">官话之三 </a><span>河南 山东 皖北 苏北</span></p>
+        <p>B4 <a href="B4.JPG">官话之四 </a><span>陕西省 甘肃省 青海省 宁夏回族自治区</span></p>
+        <p>B5 <a href="B5.JPG">官话之五 </a><span>新疆维吾尔自治区</span></p>
+        <p>B6 <a href="B6.JPG">官话之六 </a><span>四川省 云南省 贵州省 广西壮族自治区 湖北省西部</span></p>
+        <p>B7 <a href="B7.JPG">晋语 </a><span>山西省及其附近地区</span></p>
+        <p>B8 <a href="B8.JPG">东南地区的汉语方言</a></p>
+        <p>B9 <a href="B9.JPG">吴语 </a><span>浙江 上海 苏南</span></p>
+        <p>B10 <a href="B10.JPG">安徽南部汉语方言</a></p>
+        <p>B11 <a href="B11.JPG">江西省与湖南省的汉语方言</a></p>
+        <p>B12 <a href="B12.JPG">闽语 </a><span>福建 台湾 粤东 海南</span></p>
+        <p>B13 <a href="B13.JPG">广东省的汉语方言</a></p>
+        <p>B14 <a href="B14.JPG">广西壮族自治区汉语方言</a></p>
+        <p>B15 <a href="B15.JPG">客家话</a></p>
+        <p>B16 <span>海外汉语方言</span><a href="B16A.JPG">（a）太平洋地区</a> <a href="B16B.JPG">（b）其他地区</a></p>
+
+        <p>C1 <a href="C1.JPG">中国北方少数民族语言图</a></p>
+        <p>C2 <a href="C2.JPG">蒙古语族语言图</a></p>
+        <p>C3 <a href="C3.JPG">蒙古语方言图</a></p>
+        <p>C4 <a href="C4.JPG">突厥语族语言图</a></p>
+        <p>C5 <a href="C5.JPG">满通古斯语族语言图</a></p>
+        <p>C6 <a href="C6.JPG">中国南方少数民族语言图</a></p>
+        <p>C7 <a href="C7.JPG">壮侗语族语言图</a></p>
+        <p>C8 <a href="C8.JPG">苗瑶语族语言图</a></p>
+        <p>C9 <a href="C9.JPG">苗语方言图</a></p>
+        <p>C10 <a href="C10.JPG">藏缅语族语言图</a></p>
+        <p>C11 <a href="C11.JPG">藏语方言图</a></p>
+        <p>C12 <a href="C12.JPG">广西壮族自治区少数民族语言图</a></p>
+        <p>C13 <a href="C13.JPG">云南省少数民族语言图</a></p>
+        <p>C14 <a href="C14.JPG">海南岛和台湾省少数民族语言图</a></p>
+    </div>
+</template>
+ 
